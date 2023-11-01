24ced9c658eda62b7440bfc4dc12257df05e9a28
Ícaro Motta <icaromotta@unifei.edu.br>
2023-11-01 10:36:43 -0300

    feat: adicionando layout

---
+++ src/components/layout/footer.tsx
@@ -0,0 +1,7 @@
+export const Footer = () => {
+    return <footer className='mt-auto flex items-center justify-center bg-bg-dark p-3'>
+        <div className='text-gray-500 text-sm'>
+            © 2021 SociLink
+        </div>
+    </footer>
+}
---
+++ src/components/layout/navbar.tsx
@@ -0,0 +1,9 @@
+export const Navbar = () => {
+    return <nav className='sticky top-0 flex items-center justify-around bg-bg-dark p-3'>
+        <div>SociLink</div>
+
+        <div className='flex gap-7' >
+            <a href='/'>Home</a>
+        </div>
+    </nav>
+}
