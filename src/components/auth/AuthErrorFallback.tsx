3ee69f02844fd5c66668dcfb69705f3f546f6b91
Ícaro Motta <icaromotta@unifei.edu.br>
2023-11-22 09:55:01 -0300

    fix: corrigindo fallback de autenticação

--- src/components/auth/AuthErrorFallback.tsx
+++ src/components/auth/AuthErrorFallback.tsx
@@ -2,13 +2,15 @@
 
 import { Button } from '@/components/ui/button';
 
-export const AuthErrorFallback = ({error, reset}: { error: Error; reset: () => void }) => {
+export const AuthErrorFallback = (props: { error: Error; reset: () => void }) => {
+  if (!props) return null;
+
   return (
     <div>
       <div className='text-2xl'>Ocorreu um erro ao tentar realizar essa ação</div>
-      <div className='text-sm'>{error.message}</div>
+      <div className='text-sm'>{props.error?.message}</div>
 
-      <Button onClick={reset}>
+      <Button onClick={props.reset}>
         Tentar novamente
       </Button>
     </div>
